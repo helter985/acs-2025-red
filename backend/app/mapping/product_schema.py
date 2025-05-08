a73da8a2c908dff8fb01eae87a220a60027aec28
maximo <maximooriginal@gmail.com>
2025-05-08 17:52:44 -0300

    Modified Product Schema & Model

--- backend/app/mapping/product_schema.py
+++ backend/app/mapping/product_schema.py
@@ -1,6 +1,8 @@
 from marshmallow import Schema, fields
 
 class ProductSchema(Schema):
-    id = fields.Int(dump_only=True)
+    code = fields.Int(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
+    image_url = fields.Str(required=True)
+    last_updated = fields.DateTime(dump_only=True)
--- backend/app/models/product_model.py
+++ backend/app/models/product_model.py
@@ -5,6 +5,8 @@ db = SQLAlchemy()
 class Product(db.Model):
     __tablename__ = 'products'
 
-    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Float, nullable=False)
+    image_url = db.Column(db.String(200), nullable=False)
+    last_updated = db.Column(db.DateTime, nullable=False, server_default=db.func.now(), onupdate=db.func.now())
